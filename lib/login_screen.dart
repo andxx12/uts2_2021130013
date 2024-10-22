7f5f572fa2f790d1157525bbf70801ea3e004444
Andrew Limasaputra <andgxx1234@gmail.com>
2024-10-23 00:44:25 +0700

    first commit

---
+++ lib/login_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+//import 'product_screen.dart';
+
+class LoginScreen extends StatelessWidget {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _usernameController,
+              decoration: const InputDecoration(labelText: "Username"),
+            ),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(labelText: "Password"),
+              obscureText: true,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Implement logic for login authentication
+                // Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => ProductScreen()),);
+              },
+              child: const Text("Login"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
