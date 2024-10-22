026613d2c3f4a741b8904179bb6d6e92d789dab0
Andrew Limasaputra <andgxx1234@gmail.com>
2024-10-23 02:38:42 +0700

    new login layout

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,40 +1,92 @@
 import 'package:flutter/material.dart';
-//import 'product_screen.dart';
+import 'package:uts_2021130013/product_screen.dart';
 
 class LoginScreen extends StatelessWidget {
-  final TextEditingController _usernameController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-
-  LoginScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Login"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _usernameController,
-              decoration: const InputDecoration(labelText: "Username"),
-            ),
-            TextField(
-              controller: _passwordController,
-              decoration: const InputDecoration(labelText: "Password"),
-              obscureText: true,
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Implement logic for login authentication
-                // Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => ProductScreen()),);
-              },
-              child: const Text("Login"),
-            )
-          ],
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // Papan Foto (Profile Picture)
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage(
+                    'assets/https://i.pinimg.com/564x/af/d0/21/afd021919d9ae93e7491fd79cbedce27.jpg'), // ganti dengan gambar yang Anda inginkan
+                backgroundColor: Colors.transparent,
+              ),
+              SizedBox(height: 30.0),
+
+              //Text Title
+              Text(
+                'Wellcome to Nekoshop',
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+
+              // Form Email
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  floatingLabelBehavior: FloatingLabelBehavior.auto,
+                  prefixIcon: Icon(Icons.email),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20.0),
+
+              // Form Password
+              TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  floatingLabelBehavior: FloatingLabelBehavior.auto,
+                  prefixIcon: Icon(Icons.lock),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                ),
+              ),
+              SizedBox(height: 30.0),
+
+              // Tombol Login
+              ElevatedButton(
+                onPressed: () {
+                  // Aksi login
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => ProductScreen()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 50.0, vertical: 15.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                ),
+                child: Text(
+                  'Login',
+                  style: TextStyle(fontSize: 18.0),
+                ),
+              ),
+
+              SizedBox(height: 20.0),
+            ],
+          ),
         ),
       ),
     );
