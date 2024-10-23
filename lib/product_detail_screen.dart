43018e75c0599688fa974ead3e508d8cbf6c5406
Andrew Limasaputra <andgxx1234@gmail.com>
2024-10-23 23:57:46 +0700

    add button to chart

--- lib/product_detail_screen.dart
+++ lib/product_detail_screen.dart
@@ -24,136 +24,137 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
       appBar: AppBar(
         title: Text('Product Detail'),
       ),
-      body: Stack(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          SingleChildScrollView(
-            child: Column(
-              children: [
-                // Gambar produk
-                //Image.network(
-                //idget.productImage,
-                //height: 300,
-                //width: double.infinity,
-                //fit: BoxFit.cover,
-                //),
-                // Judul produk dan harga
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        widget.productName,
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(height: 8),
-                      Text(
-                        widget.productPrice,
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.green,
-                        ),
-                      ),
-                      SizedBox(height: 16),
-                      // Caption detail produk
-                      Text(
-                        'Detail Produk',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(height: 8),
-                      Text(
-                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
-                        'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
-                        'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris '
-                        'nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in '
-                        'reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. '
-                        'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia '
-                        'deserunt mollit anim id est laborum.',
-                        style: TextStyle(fontSize: 16),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          // Button Add to Cart dan jumlah barang di bagian bawah
-          Positioned(
-            bottom: 10,
-            left: 0,
-            right: 0,
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
                 children: [
-                  // Bagian untuk tambah dan kurang jumlah barang
-                  Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: Row(
+                  // Gambar produk
+                  //Image.network(
+                  //widget.productImage,
+                  //height: 300,
+                  //width: double.infinity,
+                  //fit: BoxFit.cover,
+                  //),
+                  // Judul produk dan harga
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        // Tombol kurang (-)
-                        IconButton(
-                          icon: Icon(Icons.remove),
-                          onPressed: () {
-                            setState(() {
-                              if (quantity > 1) quantity--;
-                            });
-                          },
-                        ),
-                        // Jumlah barang
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 16),
-                          child: Text(
-                            '$quantity',
-                            style: TextStyle(fontSize: 18),
+                        Text(
+                          widget.productName,
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
-                        // Tombol tambah (+)
-                        IconButton(
-                          icon: Icon(Icons.add),
-                          onPressed: () {
-                            setState(() {
-                              quantity++;
-                            });
-                          },
+                        SizedBox(height: 8),
+                        Text(
+                          widget.productPrice,
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.green,
+                          ),
+                        ),
+                        SizedBox(height: 16),
+                        // Caption detail produk
+                        Text(
+                          'Detail Produk',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
+                          'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+                          'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris '
+                          'nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in '
+                          'reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. '
+                          'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia '
+                          'deserunt mollit anim id est laborum.',
+                          style: TextStyle(fontSize: 16),
                         ),
                       ],
                     ),
                   ),
-                  // Tombol Add to Cart
-                  ElevatedButton(
-                    onPressed: () {
-                      // Fungsi yang dijalankan saat tombol ditekan
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                            content: Text(
-                                '${widget.productName} ($quantity item) has been added to your cart!')),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: Size(150, 50),
-                      backgroundColor: Colors.orange,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                    ),
-                    child: Text(
-                      'Add to Cart',
-                      style: TextStyle(fontSize: 18),
-                    ),
-                  ),
                 ],
               ),
+            ),
+          ),
+          // Bagian bawah dengan tombol tambah/kurang dan Add to Cart
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                // Bagian untuk tambah dan kurang jumlah barang
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Row(
+                    children: [
+                      // Tombol kurang (-)
+                      IconButton(
+                        icon: Icon(Icons.remove),
+                        onPressed: () {
+                          setState(() {
+                            if (quantity > 1) quantity--;
+                          });
+                        },
+                      ),
+                      // Jumlah barang
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16),
+                        child: Text(
+                          '$quantity',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                      // Tombol tambah (+)
+                      IconButton(
+                        icon: Icon(Icons.add),
+                        onPressed: () {
+                          setState(() {
+                            quantity++;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                // Tombol Add to Cart
+                ElevatedButton(
+                  onPressed: () {
+                    // Fungsi yang dijalankan saat tombol ditekan
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                          content: Text(
+                              '${widget.productName} ($quantity item) has been added to your cart!')),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(150, 50),
+                    backgroundColor: const Color.fromARGB(255, 48, 119, 233),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                  child: Text(
+                    'Add to Cart',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
